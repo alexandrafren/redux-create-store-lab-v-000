5a848140dab8e9cb0b8c0f02ab578517a940525c
Alexandra Fren <alexandra.fren@gmail.com>
2019-03-13 19:31:17 +0000

    Automatically backed up by Learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,20 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function dispatch(action = {type: '@@INIT'}){
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState(){
+    return state;
+  }
+
+  return { dispatch, getState }
 }
 
 function render() {
   const container = document.getElementById('container');
 }
+
+let store = createStore(reducer)
